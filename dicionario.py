3becf9ad9e2af730138f2a1da0e3821b258e01cc
Stenio Junior <56360594+stenionljunior@users.noreply.github.com>
2020-11-23 13:44:02 -0300

    Create dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,10 @@
+dicionario = {'nome': 'Stenio', 'idade': 27}
+
+## todos os valores: 
+print(dicionario)
+
+## acessar apenas o valor da chave nome
+print(dicionario['nome'])
+
+## acessar apenas o valor da chave idade
+print(dicionario['idade'])
